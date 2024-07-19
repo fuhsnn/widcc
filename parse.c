4d2a8d325232d588f953c299da27ce8ef2bdb352
fuhsnn <66062782+fuhsnn@users.noreply.github.com>
2024-07-19 18:50:38 +0800

    Refactor

--- parse.c
+++ parse.c
@@ -1823,7 +1823,7 @@ static Node *compound_stmt(Token **rest, Token *tok, NodeKind kind) {
   node->target_vla = current_vla;
   enter_scope();
 
-  for (; !equal(tok, "}"); add_type(cur)) {
+  while (!equal(tok, "}")) {
     if (equal(tok, "_Static_assert")) {
       static_assertion(&tok, tok->next);
       continue;
@@ -1835,8 +1835,10 @@ static Node *compound_stmt(Token **rest, Token *tok, NodeKind kind) {
 
       if (attr.is_typedef) {
         Node *expr = parse_typedef(&tok, tok, basety);
-        if (expr)
+        if (expr) {
           cur = cur->next = new_unary(ND_EXPR_STMT, expr, tok);
+          add_type(cur);
+        }
         continue;
       }
 
@@ -1846,11 +1848,14 @@ static Node *compound_stmt(Token **rest, Token *tok, NodeKind kind) {
       }
 
       Node *expr = declaration(&tok, tok, basety, &attr);
-      if (expr)
+      if (expr) {
         cur = cur->next = new_unary(ND_EXPR_STMT, expr, tok);
+        add_type(cur);
+      }
       continue;
     }
     cur = cur->next = stmt(&tok, tok, false);
+    add_type(cur);
   }
 
   node->top_vla = current_vla;
