dae1e0eba1bfb96360b83c8ae87a99a297b78e58
fuhsnn <66062782+fuhsnn@users.noreply.github.com>
2024-07-20 03:08:29 +0800

    Add helper sed script

---
+++ add_wl_pic.sh
@@ -0,0 +1,3 @@
+sed -i 's/^\s*lt_prog_compiler_wl=$/lt_prog_compiler_wl=-Wl,/g' $1
+sed -i 's/^\s*lt_prog_compiler_pic=$/lt_prog_compiler_pic=-fPIC/g' $1
+sed -i 's/^\s*lt_prog_compiler_static=$/lt_prog_compiler_static=-static/g' $1
