3e585f2e24d2996c974e718d978de07548884cff
fuhsnn <66062782+fuhsnn@users.noreply.github.com>
2025-06-02 02:42:30 +0800

    Remove support for universal character names

--- test/unicode.c
+++ test/unicode.c
@@ -12,11 +12,6 @@ int main() {
   ASSERT(4, sizeof(L'\0'));
   ASSERT(97, L'a');
 
-  ASSERT(0, strcmp("αβγ", "\u03B1\u03B2\u03B3"));
-  ASSERT(0, strcmp("日本語", "\u65E5\u672C\u8A9E"));
-  ASSERT(0, strcmp("日本語", "\U000065E5\U0000672C\U00008A9E"));
-  ASSERT(0, strcmp("🌮", "\U0001F32E"));
-
   ASSERT(-1, L'\xffffffff'>>31);
   ASSERT(946, L'β');
   ASSERT(12354, L'あ');
